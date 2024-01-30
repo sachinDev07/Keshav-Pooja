2d6c08b24381d75bd913ef474f32224ccc267b2d
Ashish <ashishgujjar277@gmail.com>
2024-01-30 21:25:47 +0530

    database connect

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,7 +4,7 @@ const { DB_NAME } = require("./constant");
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        await mongoose.connect(`mongodb+srv://sachinkrj369:xKQ9xuj2muHaNBqV@cluster0.e8d1zph.mongodb.net/${DB_NAME}`)
     } catch (error) {
         console.error("Mongodb connnection error: " + error);
     }
--- backend/src/index.js
+++ backend/src/index.js
@@ -10,7 +10,15 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors( {
+  origin:["https://deploy-mern-1whq.vercel.app"],
+  methods:["POST","GET"],
+  credentials:true
+}));
+
+app.get("/", (req, res) => {
+  res.json("Hello");
+})
 
 app.use("/url", userRoutes);
 

87b473c50afb1db075aeb465f5a20a4f5884ed9e
Ashish <ashishgujjar277@gmail.com>
2024-01-30 17:16:34 +0530

    complete Backend

--- app.js
+++ app.js
@@ -244,3 +244,25 @@ const audio = (() => {
     };
 })();
 
+
+const handleInput = (event) => {
+    event.preventDefault();
+
+    const formNama = document.getElementById('form-nama');
+    const formPesan = document.getElementById('form-pesan');
+
+    if(formNama.value === "" | formPesan.value == "") {
+        const EmptyFiled = document.getElementById('empty');
+        EmptyFiled.classList.add("show");
+        setTimeout(() => {
+            EmptyFiled.classList.remove("show");
+        }, 3000);
+    }
+    else {
+        const ThanksField = document.getElementById('thanks');
+        ThanksField.classList.add("show");
+        setTimeout(() => {
+            ThanksField.classList.remove("show");
+        }, 3000);
+    }
+}

cc4f46d2dbfcda538cda073ec3c42dcd0e043bd9
Ashish <ashishgujjar277@gmail.com>
2024-01-31 20:49:27 +0530

    fixing

--- backend/src/index.js
+++ backend/src/index.js
@@ -10,11 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors( {
-  origin:["https://deploy-mern-1whq.vercel.app"],
-  methods:["POST","GET"],
-  credentials:true
-}));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.json("Hello");
